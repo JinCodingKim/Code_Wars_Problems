868b3ed3cbabc296a6956bcfd8d89d1346c31862
unknown <jinyeob.kim.0818@gmail.com>
2018-07-06 20:28:44 -0500

    IQ Test

--- CodeWars.jsx
+++ CodeWars.jsx
@@ -488,3 +488,31 @@ function highestRank(arr) {
     .sort((a, b) => b[0] - a[0]);
   return Number(topNumbers[0][0]);
 }
+
+/////////////
+// IQ Test //
+/////////////
+
+// Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers differs from the others. Bob observed that one number usually differs from the others in evenness. Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
+
+// ! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)
+
+// Examples :
+
+// iqTest("2 4 7 8 10") => 3 // Third number is odd, while the rest of the numbers are even
+
+// iqTest("1 2 1 1") => 2 // Second number is even, while the rest of the numbers are odd
+
+// Solution :
+
+function iqTest(numbers) {
+  let evens = [];
+  let odds = [];
+  numbers
+    .split(" ")
+    .map(
+      (num, i) =>
+        parseInt(num) % 2 === 0 ? evens.push(i + 1) : odds.push(i + 1)
+    );
+  return evens.length > odds.length ? odds[0] : evens[0];
+}
