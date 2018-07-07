aceb2e6636d3703ffbd7de6356954025519a01fe
unknown <jinyeob.kim.0818@gmail.com>
2018-07-06 20:50:14 -0500

    extract file name

--- CodeWars.jsx
+++ CodeWars.jsx
@@ -516,3 +516,48 @@ function iqTest(numbers) {
     );
   return evens.length > odds.length ? odds[0] : evens[0];
 }
+
+///////////////////////
+// extract file name //
+///////////////////////
+
+// You have to extract a portion of the file name as follows:
+
+// Assume it will start with date represented as long number
+// Followed by an underscore
+// Youll have then a filename with an extension
+// it will always have an extra extension at the end
+// Inputs:
+// 1231231223123131_FILE_NAME.EXTENSION.OTHEREXTENSION
+
+// 1_This_is_an_otherExample.mpg.OTHEREXTENSIONadasdassdassds34
+
+// 1231231223123131_myFile.tar.gz2
+// Outputs
+// FILE_NAME.EXTENSION
+
+// This_is_an_otherExample.mpg
+
+// myFile.tar
+// The recommend way to solve it is using RegEx and specifically groups.
+
+// Solution :
+
+class FileNameExtractor {
+  static extractFileName(dirtyFileName) {
+    let fileArr = dirtyFileName.split("_").splice(1);
+    let sepArr;
+    if (fileArr.length > 1) {
+      sepArr = fileArr[1]
+        .split(".")
+        .splice(0, 2)
+        .join(".");
+      return [fileArr[0], sepArr].join("_");
+    } else {
+      return fileArr[0]
+        .split(".")
+        .splice(0, 2)
+        .join(".");
+    }
+  }
+}
